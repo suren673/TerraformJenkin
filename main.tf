578b7b11c14d85627058e84806e9625f972a2566
Surender Singh <surendersingh@Surenders-MacBook-Air.local>
2021-05-20 23:40:57 -0500

    added

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+  zone    = var.gcp_zone
+ credentials = var.gcp_auth_file
+}
+
+resource "google_compute_instance" "default" {
+  name= var.gcp_instancename
+  machine_type= var.gcp_machinetype
+
+  boot_disk {
+      initialize_params{
+          image="debian-cloud/debian-9"
+      }
+  }
+network_interface {
+    network="default"
+
+    access_config{
+
+    }
+}
+
+
+}
